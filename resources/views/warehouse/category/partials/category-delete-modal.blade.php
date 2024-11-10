2d1b4a7a69c2a5277a9df3628787a9beae6709d3
StPronk <stpronk@gmail.com>
2024-11-10 15:41:10 +0100

    Fixed typo in delete modal

--- resources/views/warehouse/category/partials/category-delete-modal.blade.php
+++ resources/views/warehouse/category/partials/category-delete-modal.blade.php
@@ -4,7 +4,7 @@
         <input type="hidden" name="uuid" value="{{ $category->uuid }}">
 
         <div class="flex flex-col gap-6 pb-6">
-            <h2 class="text-lg bg-red-500 rounded-lg text-white py-4 px-8 avenir">Are you sure you want to delete the brand <b>"{{ $category->name }}"</b>?</h2>
+            <h2 class="text-lg bg-red-500 rounded-lg text-white py-4 px-8 avenir">Are you sure you want to delete the category <b>"{{ $category->name }}"</b>?</h2>
             <ul class="list-disc pl-6">
                 <li>All linked items will be set to null</li>
                 <li>This action cannot be un-done</li>
