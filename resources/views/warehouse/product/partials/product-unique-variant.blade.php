15792f9cea608f305e2cb79bcb277723627dfa07
Steve <s.pronk1@dictu.nl>
2024-11-11 15:24:04 +0100

    Made separate includes for product variant types

---
+++ resources/views/warehouse/product/partials/product-unique-variant.blade.php
@@ -0,0 +1,7 @@
+<div class="flex flex-col gap-4">
+    <div class="flex flex-row justify-between">
+        <span class='block avenir-bold text-lg text-gray-700 my-auto ml-2'>
+            {{ __("Product variant") }}
+        </span>
+    </div>
+</div>
