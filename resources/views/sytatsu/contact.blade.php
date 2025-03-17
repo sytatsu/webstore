5c14f2770022696176fc87dfdfc7678b10617259
Steve <s.pronk1@dictu.nl>
2025-03-17 11:10:34 +0100

    Removed commented code & ended div correctly

--- resources/views/sytatsu/contact.blade.php
+++ resources/views/sytatsu/contact.blade.php
@@ -11,49 +11,6 @@
     </div>
 
     <div class="mt-12 max-w-lg mx-auto">
-
         <livewire:sytatsu.components.contact-form />
-
-{{--        <!-- Card -->--}}
-{{--        <div class="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8 bg-background-light dark:bg-slate-800 dark:border-neutral-700 shadow-lg dark:shadow-primary" >--}}
-{{--            <div class="grid gap-4 lg:gap-6">--}}
-{{--                <div>--}}
-{{--                    <label for="hs-fullname-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium dark:text-white">{{ __('Name') }}</label>--}}
-{{--                    <input type="text" name="hs-fullname-contacts-1" id="hs-fullname-contacts-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">--}}
-{{--                </div>--}}
-
-{{--                <!-- Grid -->--}}
-{{--                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">--}}
-{{--                    <div>--}}
-{{--                        <label for="hs-email-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium dark:text-white">{{ __('Email') }}</label>--}}
-{{--                        <input type="email" name="hs-email-contacts-1" id="hs-email-contacts-1" autocomplete="email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">--}}
-{{--                    </div>--}}
-
-{{--                    <div>--}}
-{{--                        <label for="hs-phone-number-1" class="block mb-2 text-sm text-gray-700 font-medium dark:text-white">{{ __('Phone Number') }}</label>--}}
-{{--                        <input type="text" name="hs-phone-number-1" id="hs-phone-number-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <!-- End Grid -->--}}
-
-{{--                <div>--}}
-{{--                    <label for="hs-about-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium dark:text-white">{{ __('Details') }}</label>--}}
-{{--                    <textarea id="hs-about-contacts-1" name="hs-about-contacts-1" rows="4" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"></textarea>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <!-- End Grid -->--}}
-
-{{--            <div class="mt-6 grid">--}}
-{{--                <button wire:loading.attr="disabled" type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary-dark text-white hover:bg-primary focus:outline-none focus:bg-primary disabled:opacity-50 disabled:pointer-events-none">--}}
-{{--                    {{ __('Send') }} <i class="fa fa-paper-plane"></i>--}}
-{{--                </button>--}}
-{{--            </div>--}}
-
-{{--            <div class="mt-3 text-center">--}}
-{{--                <p class="text-sm text-gray-500 dark:text-neutral-500">--}}
-{{--                    {{ __('We\'ll get back to you as soon as possible.') }}--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-        <!-- End Card -->
     </div>
+</div>
