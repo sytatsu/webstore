661621abb4e992dbcdf9f1883a37c1537ade57eb
StPronk <stpronk@gmail.com>
2025-06-15 13:43:01 +0200

    Added about us page

--- app/Services/LayoutService.php
+++ app/Services/LayoutService.php
@@ -19,7 +19,7 @@ class LayoutService
             return $appName;
         }
 
-        return __($title) . " | $appName";
+        return __($title) . " - $appName";
     }
 
     public function render(string $view, string $layout, array $viewAttributes = [], array $layoutAttributes = [])
