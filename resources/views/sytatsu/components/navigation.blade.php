4287179140487368b3663afbb99cb451995591e8
StPronk <stpronk@gmail.com>
2025-06-15 19:49:50 +0200

    Fixed mobile navigation issue

--- resources/views/sytatsu/components/navigation.blade.php
+++ resources/views/sytatsu/components/navigation.blade.php
@@ -23,24 +23,24 @@
                     <!-- End Offcanvas Header -->
                     <div class="grow">
                         <div class="flex flex-col md:flex-row md:justify-center md:items-center gap-0.5 md:gap-2">
-                            <a class="px-1 m-3 md:m-0 flex items-center text-sm text-gray-800 border-b-2 border-transparent hover:!border-secondary dark:text-neutral-200
-                                @if (Route::is('sytatsu.welcome')) !border-primary @endif"
+                            <a class="px-1 m-3 md:m-0 flex items-center text-sm text-gray-800 border-b-2 border-transparent hover:md:!border-secondary dark:text-neutral-200"
                                href="{{ route('sytatsu.welcome')  }}"
-                            ><svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden pr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
-                                <i class="fa fa-house pb-1"></i>
+                            >
+                                <img src="{{ asset('/images/brands/no_background_text_only.webp') }}" alt="brand" width="70" class="md:hidden">
+                                <span class="hidden md:block"><i class="fa fa-house p-1"></i></span>
                             </a>
 
                             <a class="px-1 m-3 md:m-0 flex items-center text-sm text-gray-800 border-b-2 border-transparent hover:!border-secondary dark:text-neutral-200
                                @if (Route::is('sytatsu.about')) !border-primary @endif"
                                href="{{ route('sytatsu.about')  }}"
-                            ><svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden pr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                            >
                                 {{ __('About us') }}
                             </a>
 
                             <a class="px-1 m-3 md:m-0 flex items-center text-sm text-gray-800 border-b-2 border-transparent hover:!border-secondary dark:text-neutral-200
                                @if (Route::is('sytatsu.contact')) !border-primary @endif"
                                href="{{ route('sytatsu.contact')  }}"
-                            ><svg class="shrink-0 size-4 me-3 md:me-2 block md:hidden pr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                            >
                                 {{ __('Contact') }}
                             </a>
                         </div>
