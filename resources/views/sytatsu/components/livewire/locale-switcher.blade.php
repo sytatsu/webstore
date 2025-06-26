c1b1a5f9f9b657f2d1feaf4e465d19d7ab533332
StPronk <stpronk@gmail.com>
2025-06-26 23:12:40 +0200

    Make sure locale switch is always set correctly

--- resources/views/sytatsu/components/livewire/locale-switcher.blade.php
+++ resources/views/sytatsu/components/livewire/locale-switcher.blade.php
@@ -1,11 +1,11 @@
 <div class="flex flex-row rounded-lg">
     @foreach($supportedLocales as $supportedLocale => $supportedLocaleAttributes)
         @if($supportedLocale === $activeLocale['locale'])
-            <div class="p-0.5 border-2 border border-primary rounded-full">
+            <div class="p-0.5 border-2 border border-primary rounded-full my-auto">
                 <img src="{{ Vite::asset($activeLocale['image']) }}" alt="{{ $activeLocale['tooltip'] }}" width="24px" height="24px" />
             </div>
         @else
-            <button type="button" wire:key="{{ $supportedLocale }}" wire:click.prevent="switchLocale('{{ $supportedLocale }}')" class="cursor-pointer flex flex-row justify-between p-0.5 border-2 border-transparent rounded-full">
+            <button type="button" wire:key="{{ $supportedLocale }}" wire:click.prevent="switchLocale('{{ $supportedLocale }}')" class="cursor-pointer flex flex-row justify-between p-0.5 border-2 border-transparent rounded-full my-auto">
                 <img src="{{ Vite::asset($supportedLocaleAttributes['image']) }}" alt="{{ $supportedLocaleAttributes['tooltip'] }}" width="24px" height="24px" />
             </button>
         @endif
