9f53fa0bf7b794197341cdadea06002cadfce09b
StPronk <stpronk@gmail.com>
2025-07-01 10:51:03 +0200

    Altered navigation to be easily used with webshop in the near future

--- resources/views/layouts/sytatsu-layout.blade.php
+++ resources/views/layouts/sytatsu-layout.blade.php
@@ -42,48 +42,7 @@
     </head>
 
     <body class="bg-gradient-to-br from-[#FFF1EA] dark:from-[#12100E] from-10% to-[#FFFFFF] dark:to-[#2B4162] to-90% bg-no-repeat min-h-screen flex flex-col justify-between">
-
-        {{------ ABSOLUTE TOP ------}}
-
-
         @include('sytatsu.components.navigation')
-
-
-        <div class="absolute top-0 right-0 p-5">
-            <div class="flex flex-row-reverse gap-4">
-                <livewire:sytatsu.components.locale-switcher />
-
-                <button type="button"
-                        class="hs-dark-mode-active:hidden flex hs-dark-mode font-medium text-gray-800 rounded-full focus:outline-none dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                        data-hs-theme-click-value="dark">
-                  <span class="group inline-flex shrink-0 justify-center items-center size-9 my-auto">
-                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-                    </svg>
-                  </span>
-                </button>
-                <button type="button"
-                        class="hs-dark-mode-active:flex hidden hs-dark-mode font-medium text-gray-800 rounded-full focus:outline-none dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                        data-hs-theme-click-value="light">
-                  <span class="group inline-flex shrink-0 justify-center items-center size-9 my-auto">
-                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                      <circle cx="12" cy="12" r="4"></circle>
-                      <path d="M12 2v2"></path>
-                      <path d="M12 20v2"></path>
-                      <path d="m4.93 4.93 1.41 1.41"></path>
-                      <path d="m17.66 17.66 1.41 1.41"></path>
-                      <path d="M2 12h2"></path>
-                      <path d="M20 12h2"></path>
-                      <path d="m6.34 17.66-1.41 1.41"></path>
-                      <path d="m19.07 4.93-1.41 1.41"></path>
-                    </svg>
-                  </span>
-                </button>
-            </div>
-        </div>
-        {{------ END ABSOLUTE TOP ------}}
 
         {{------ LAYOUT ------}}
         <div class="flex flex-col flex-grow
