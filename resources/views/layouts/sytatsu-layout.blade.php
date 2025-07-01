436802539e913acb0245ec9d939f7927061c0f22
StPronk <stpronk@gmail.com>
2025-07-01 10:53:28 +0200

    Set footer to own blade component

--- resources/views/layouts/sytatsu-layout.blade.php
+++ resources/views/layouts/sytatsu-layout.blade.php
@@ -56,12 +56,6 @@
         </div>
         {{------ END LAYOUT ------}}
 
-        {{------ FOOTER ------}}
-        <div class="sticky-bottom">
-            <p class="flex justify-center mx-auto my-0 text-secondary-light py-4">
-                {!! __('partials/footer.creator', ['creator' => '<a class="ml-1 underline text-secondary font-bold" href="https://stpronk.nl/">stpronk.nl</a>']) !!}
-            </p>
-        </div>
-        {{------ END FOOTER ------}}
+        @include('sytatsu.components.footer')
     </body>
 </html>
