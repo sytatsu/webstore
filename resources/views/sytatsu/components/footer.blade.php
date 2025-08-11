8f2cb7d83ea14d20c4a4e1e5af9b1572ab400fe3
StPronk <stpronk@gmail.com>
2025-08-11 23:02:32 +0200

    Added footer copyright & formatted translation

--- resources/views/sytatsu/components/footer.blade.php
+++ resources/views/sytatsu/components/footer.blade.php
@@ -1,5 +1,9 @@
-<div class="sticky-bottom bg-slate-50 dark:bg-slate-700 shadow-inner">
-    <p class="flex justify-center mx-auto my-0 text-secondary-light py-4">
+<div class="flex flex-col gap-6 sticky-bottom bg-slate-50 dark:bg-slate-700 shadow-inner py-4">
+    <p class="flex justify-center mx-auto my-0 text-secondary-light">
         {!! __('partials/footer.creator', ['creator' => '<a class="ml-1 underline text-secondary font-bold" href="https://stpronk.nl/">stpronk.nl</a>']) !!}
+    </p>
+
+    <p class="flex justify-center text-sm mx-auto my-0 text-slate-400 dark:text-slate-300">
+        Copyright © {{ \Carbon\Carbon::now()->format('Y') }} - Sytatsu - {{ __('All rights reserved') }}
     </p>
 </div>
