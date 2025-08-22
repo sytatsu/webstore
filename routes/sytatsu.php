cfff199037ae6c1d91c45c75e2fd7d3195487741
StPronk <stpronk@gmail.com>
2025-08-22 16:37:53 +0200

    NPM Build

--- routes/sytatsu.php
+++ routes/sytatsu.php
@@ -24,7 +24,7 @@ Route::get('/contact', LivewireSytatsu\Contact::class)->name('sytatsu.contact');
 Route::get('/webstore', LivewireSytatsu\Webstore\Welcome::class)->name('sytatsu.webstore.welcome');
 Route::get('/products/{product}', LivewireSytatsu\Webstore\ProductPage::class)->name('sytatsu.webstore.product');
 Route::get('/collections/{collection}', LivewireSytatsu\Webstore\CollectionPage::class)->name('sytatsu.webstore.collection');
-
+Route::get('/checkout', LivewireSytatsu\Webstore\CheckoutPage::class)->name('sytatsu.webstore.checkout');
 
 Route::model('product', Product::class, function (string $slug) {
     return ($element = Url::query()->where('slug', $slug)->firstOrFail()->element) instanceof Product ? $element : abort(404, 'Element not Product');
