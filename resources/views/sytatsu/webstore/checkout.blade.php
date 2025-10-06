b89c345f9d35c90c2e1f2b1757d3475441f429d3
StPronk <stpronk@gmail.com>
2025-10-06 07:51:59 +0200

    Move

--- resources/views/sytatsu/webstore/checkout.blade.php
+++ resources/views/sytatsu/webstore/checkout.blade.php
@@ -1,9 +1,10 @@
 <div class="min-h-screen grid grid-cols-3">
-    <div class="col-span-2">
+    <div class="col-span-2 p-8 flex items-center justify-center max-h-screen overflow-y-scroll shadow-md">
         <livewire:sytatsu.components.checkout-form/>
     </div>
 
-    <div class="col-span-1 bg-slate-50 dark:bg-slate-800">
+    <div class="col-span-1 p-8 flex items-center justify-items-stretch bg-slate-50 dark:bg-slate-800">
+        {{-- TODO: create new cart component with same style but different functionality--}}
         <livewire:sytatsu.components.cart.cart-details :checkout="true"/>
     </div>
 </div>
