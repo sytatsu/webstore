4ca06ca63c021f231e1cc384ed0506cf9315ee2b
StPronk <stpronk@gmail.com>
2026-04-15 22:02:46 +0200

    Corrected position

--- resources/views/sytatsu/components/livewire/collection/collection-cards.blade.php
+++ resources/views/sytatsu/components/livewire/collection/collection-cards.blade.php
@@ -1,6 +1,8 @@
 <div class="flex flex-col gap-8">
     @forelse ($this->activeCollections as $collection)
         <div class="shadow-md dark:shadow-slate-700 bg-white dark:bg-slate-800 py-8 px-6 lg:p-12">
+            <x-ui.spinner-overlay wire:loading.flex />
+
             <div class="group flex flex-row justify-between items-center mb-8 ">
                 @if($showMore)
                     <a class="text-2xl avenir-bold text-black dark:text-white hover:underline uppercase" href="{{ \App\Services\WebstoreHelperService::getCollectionRoute($collection->collection) }}">
@@ -20,7 +22,6 @@
             <hr class="mb-8 border-gray-200 dark:border-gray-500">
 
             <div class="grid {{ $gridColumns }} gap-x-4 gap-y-6 md:gap-6 lg:gap-8 xl:gap-12">
-                <x-ui.spinner-overlay wire:loading.flex />
                 @foreach($collection->products as $product)
                     <livewire:sytatsu.components.product.product-tile :product="$product" :wire:key="'product-'.$product->id.'-'.md5($product->updated_at)" />
                 @endforeach
