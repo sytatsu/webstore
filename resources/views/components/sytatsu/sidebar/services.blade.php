37bff16d1706f3283e68525f6d2eb5e4066b83f8
StPronk <stpronk@gmail.com>
2026-04-15 20:38:28 +0200

    Fixed copy paste issue

--- resources/views/components/sytatsu/sidebar/services.blade.php
+++ resources/views/components/sytatsu/sidebar/services.blade.php
@@ -1,15 +1,15 @@
-<x-ui.card-box title="{{ __(‘Our Services’) }}" class="text-sm">
+<x-ui.card-box title="{{ __('Our Services') }}" class="text-sm">
     <p class="mb-6 text-gray-600 dark:text-neutral-400">
-        {{ __(‘Whether you need a custom print or professional 3D printer maintenance, we’re here to help you get the best results.’) }}
+        {{ __('Whether you need a custom print or professional 3D printer maintenance, we’re here to help you get the best results.') }}
     </p>
 
     <div class="flex flex-col gap-3">
-        <x-ui.button.default.primary class="w-full text-center" href="{{ route(‘sytatsu.custom-print’) }}">
-            {{ __(‘Custom Print’) }} <i class="fa fa-print ml-2"></i>
+        <x-ui.button.default.primary class="w-full text-center" href="{{ route('sytatsu.custom-print') }}">
+            {{ __('Custom Print') }} <i class="fa fa-print ml-2"></i>
         </x-ui.button.default.primary>
 
-        <x-ui.button.default.primary class="w-full text-center" href="{{ route(‘sytatsu.maintenance-repair’) }}">
-            {{ __(‘Maintenance & Repairs’) }} <i class="fa fa-wrench ml-2"></i>
+        <x-ui.button.default.primary class="w-full text-center" href="{{ route('sytatsu.maintenance-repair') }}">
+            {{ __('Maintenance & Repairs') }} <i class="fa fa-wrench ml-2"></i>
         </x-ui.button.default.primary>
     </div>
 </x-ui.card-box>
