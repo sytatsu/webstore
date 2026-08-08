83a26b370ef25609c2cc52c0cbbba44c2113cfb1
StPronk <stpronk@gmail.com>
2026-08-08 23:01:01 +0200

    Bugfix: fixed issue with building image

--- .docker/prod/php-fpm/entrypoint.sh
+++ .docker/prod/php-fpm/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
+echo 'Discovering packages...'
+php artisan package:discover --ansi
+
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
